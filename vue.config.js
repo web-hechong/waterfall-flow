7c7652f17b1279f0aaf8af8fef4c67033d4d715a
he-chong-web <202485543@qq.com>
2026-08-18 16:34:08 +0800

    修改输出目录

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/waterfall-flow/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/waterfall-flow/' : '/',
+  outputDir: 'docs'
 })
